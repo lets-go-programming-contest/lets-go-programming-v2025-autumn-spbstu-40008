36c4e2ff20dc902f900dd23f683639b9d3493ec0
juliawheatman <julia.wheatman@gmail.com>
2025-09-22 00:44:34 +0300

    [TASK-1] Initial commit

---
+++ julia.pshenitsyna/task-1/cmd/service/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func add(a float64, b float64) {
+	time.Sleep(1 * time.Second)
+	fmt.Print(a + b)
+}
+
+func sub(a float64, b float64) {
+	time.Sleep(1 * time.Second)
+	fmt.Print(a - b)
+}
+
+func mult(a float64, b float64) {
+	time.Sleep(1 * time.Second)
+	fmt.Print(a * b)
+}
+
+func div(a float64, b float64) {
+	if b == 0 {
+		fmt.Println("Division by zero")
+		return
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Print(a / b)
+}
+
+func main() {
+	var a float64
+	var b float64
+	var op string
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	fmt.Scan(&op)
+	switch op {
+	case "+":
+		add(a, b)
+	case "-":
+		sub(a, b)
+	case "*":
+		mult(a, b)
+	case "/":
+		div(a, b)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
