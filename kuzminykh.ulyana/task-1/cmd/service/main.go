c83a9585d4f22cff963933b883428448fb859741
Ulyashkass <hukumka1978@gmail.com>
2025-09-22 00:54:38 +0300

    [TASK-1] Initial commit

---
+++ kuzminykh.ulyana/task-1/cmd/service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1, num2 int
+	var sign string
+
+	_, err1 := fmt.Scan(&num1)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err2 := fmt.Scan(&num2)
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	fmt.Scan(&sign)
+
+	switch sign {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(num1 / num2)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
