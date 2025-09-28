8266945b8a511642b3f0f41d9bca8eaa5c21bfbc
Diyar Narumov <tomasansler@gmail.com>
2025-09-28 17:59:09 +0300

    [TASK-1] Fixing errors 2

--- diyar.narumov/task-1/cmd/service/main.go
+++ diyar.narumov/task-1/cmd/service/main.go
@@ -4,38 +4,44 @@ import "fmt"
 
 func main() {
 	var a, b int
+	var op string
+
 	_, err := fmt.Scan(&a)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
+
 	_, err = fmt.Scan(&b)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-	var op string
+
 	_, err = fmt.Scan(&op)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
+
 	var result int
-	if op == "+" {
+	switch op {
+	case "+":
 		result = a + b
-	} else if op == "-" {
+	case "-":
 		result = a - b
-	} else if op == "*" {
+	case "*":
 		result = a * b
-	} else if op == "/" {
+	case "/":
 		if b == 0 {
 			fmt.Println("Division by zero")
 			return
 		}
 		result = a / b
-	} else {
+	default:
 		fmt.Println("Invalid operation")
 		return
 	}
+
 	fmt.Println(result)
 }
