219d59efaf152ac4b52e4e88c314955fd279bd54
juliawheatman <julia.wheatman@gmail.com>
2025-09-28 21:53:10 +0300

    [TASK-1] change: single module for var; add: space between code blocks

--- julia.pshenitsyna/task-1/cmd/service/main.go
+++ julia.pshenitsyna/task-1/cmd/service/main.go
@@ -4,17 +4,21 @@ import (
 	"fmt"
 )
 
+
 func add(a float64, b float64) {
 	fmt.Println(a + b)
 }
+
 
 func sub(a float64, b float64) {
 	fmt.Println(a - b)
 }
 
+
 func mult(a float64, b float64) {
 	fmt.Println(a * b)
 }
+
 
 func div(a float64, b float64) {
 	if b == 0 {
@@ -24,25 +28,37 @@ func div(a float64, b float64) {
 	fmt.Println(a / b)
 }
 
+
 func main() {
-	var a float64
-	var b float64
-	var op string
+
+	var (
+		a float64
+		b float64
+		op string
+	)
+
+
 	_, err := fmt.Scan(&a)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
+
+
 	_, err = fmt.Scan(&b)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
+
+
 	_, err = fmt.Scan(&op)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
+
+
 	switch op {
 	case "+":
 		add(a, b)
