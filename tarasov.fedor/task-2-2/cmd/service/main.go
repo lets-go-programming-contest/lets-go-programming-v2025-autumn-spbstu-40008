b18c4085916e25c10d313cca7f6435c44a7817d1
WellC2G <fedorka2010@gmail.com>
2025-09-29 23:48:06 +0300

    [TASK-2-2] edit scan maxheap

--- tarasov.fedor/task-2-2/cmd/service/main.go
+++ tarasov.fedor/task-2-2/cmd/service/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"container/heap"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 type MaxHeap []int
@@ -35,37 +32,23 @@ func main() {
 		os.Exit(0)
 	}
 
-	scanner := bufio.NewScanner(os.Stdin)
+	nums := make([]int, n)
+	for index := range n {
+		if _, err := fmt.Scan(&nums[index]); err != nil {
 
-	scanner.Scan()
-	line := scanner.Text()
-
-	parts := strings.Fields(line)
-
-	if len(parts) != n {
-		fmt.Println("Invalid number of dishes")
-		os.Exit(0)
+			return
+		}
 	}
 
 	ai := &MaxHeap{}
 
-	for _, part := range parts {
-		num, err := strconv.Atoi(part)
-		if err != nil {
-			fmt.Println("Invalid number")
-			os.Exit(0)
-		}
+	for _, num := range nums {
 		heap.Push(ai, num)
 	}
 
 	var k int
 	_, err = fmt.Scanln(&k)
-	if err != nil {
-		fmt.Println("Invalid number of prefer dish")
-		os.Exit(0)
-	}
-
-	if k > n || k <= 0 {
+	if err != nil || k > n || k <= 0 {
 		fmt.Println("Invalid number of prefer dish")
 		os.Exit(0)
 	}
