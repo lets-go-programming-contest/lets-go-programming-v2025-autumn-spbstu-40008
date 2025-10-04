d613265fea631101484fd0aba179b7df9a23b778
rabbitdfs <miroslav_ponomarenko@bk.ru>
2025-10-04 19:15:32 +0300

    [TASK-2-1] Fix lint errors

--- miroslav.ponomarenko/task-2-1/cmd/service/main.go
+++ miroslav.ponomarenko/task-2-1/cmd/service/main.go
@@ -1,28 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-
 	var (
-		y, n, minT, maxT, temp int
-		sign                   string
+		departments, employees, minT, maxT, temp int
+		sign                                     string
 	)
 
-	if _, err := fmt.Scan(&y); err != nil {
+	if _, err := fmt.Scan(&departments); err != nil {
 		return
 	}
 
-	for i := 0; i < y; i++ {
-		if _, err := fmt.Scan(&n); err != nil {
+	for range departments {
+		if _, err := fmt.Scan(&employees); err != nil {
 			return
 		}
 
 		minT = 15
 		maxT = 30
-		for j := 0; j < n; j++ {
+		for range employees {
 			if _, err := fmt.Scanf("%s %d", &sign, &temp); err != nil {
 				return
 			}
