89b03ca720039f8795b3a0fa0f5c422f79042a00
rabbitdfs <miroslav_ponomarenko@bk.ru>
2025-10-04 21:04:00 +0300

    [TASK-2-2] Fix forcetypeassert, nlreturn, wsl

--- miroslav.ponomarenko/task-2-2/cmd/service/main.go
+++ miroslav.ponomarenko/task-2-2/cmd/service/main.go
@@ -7,12 +7,16 @@ import (
 
 type Heap []int
 
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i] < h[j] }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *Heap) Len() int           { return len(*h) }
+func (h *Heap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *Heap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *Heap) Push(x any) {
-	*h = append(*h, x.(int))
+	v, ok := x.(int)
+	if !ok {
+		return
+	}
+	*h = append(*h, v)
 }
 
 func (h *Heap) Pop() any {
@@ -20,6 +24,7 @@ func (h *Heap) Pop() any {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
