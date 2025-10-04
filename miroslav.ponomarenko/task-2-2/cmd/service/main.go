ac5b4c13bf8ca46b9377046b633a6d66568dcd79
rabbitdfs <miroslav_ponomarenko@bk.ru>
2025-10-04 20:50:10 +0300

    [TASK-2-2] Initial commit (from upstream)

---
+++ miroslav.ponomarenko/task-2-2/cmd/service/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type Heap []int
+
+func (h Heap) Len() int           { return len(h) }
+func (h Heap) Less(i, j int) bool { return h[i] < h[j] }
+func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *Heap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *Heap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func main() {
+	var dishes, dish, prefer int
+
+	myHeap := &Heap{}
+	heap.Init(myHeap)
+
+	if _, err := fmt.Scanln(&dishes); err != nil {
+		return
+	}
+
+	for range dishes {
+		if _, err := fmt.Scan(&dish); err != nil {
+			return
+		}
+		heap.Push(myHeap, dish)
+	}
+
+	if _, err := fmt.Scanln(&prefer); err != nil {
+		return
+	}
+
+	for myHeap.Len() > prefer {
+		heap.Pop(myHeap)
+	}
+
+	fmt.Println((*myHeap)[0])
+}
