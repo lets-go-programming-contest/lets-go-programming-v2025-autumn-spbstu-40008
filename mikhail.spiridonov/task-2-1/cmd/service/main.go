9bf2d94deac92cfa464f0ec201be85a7ab5a98da
mordw1n <lolpovar0@gmail.com>
2025-10-06 03:43:58 +0300

    [TASK-2-1] Start realisation

--- mikhail.spiridonov/task-2-1/cmd/service/main.go
+++ mikhail.spiridonov/task-2-1/cmd/service/main.go
@@ -1,4 +1,37 @@
 package main
 
+import "fmt"
+
+const MinTempDefault = 15
+const MaxTempDefault = 30
+const ErrVal = -1
+
 func main() {
+	var countDepartments int
+	var countEmployees int
+	var temp int
+	var newMinTemperature int
+	var newMaxTemperature int
+	var setTempSign string
+
+	fmt.Scan(&countDepartments)
+	for range countDepartments {
+		
+		fmt.Scan(&countEmployees)
+		for range countEmployees {
+			fmt.Scan(&setTempSign, &temp)
+			switch setTempSign {
+			case ">=":
+				newMinTemperature = max(MaxTempDefault, temp)
+			case "<=":
+				newMaxTemperature = min(MinTempDefault, temp)
+			default:
+				continue
+			}
+			if newMinTemperature > newMaxTemperature {
+				fmt.Println(ErrVal)
+			}
+			fmt.Println(newMinTemperature)
+		}
+	}
 }
