b3d92e422807d21b1be8ab1404637853de2a96f2
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-10-07 19:02:10 +0300

    [TASK-1] bug fix

--- nikita.prokopenko/task-1/cmd/service/main.go
+++ nikita.prokopenko/task-1/cmd/service/main.go
@@ -2,48 +2,50 @@ package main
 
 import "fmt"
 
-func conclusion(information any){
+func conclusion(information any) {
 	fmt.Println(information)
 }
 
-
-func mathematicalOperations(number1 int, number2 int, theAddition string){
-	switch theAddition{
+func mathematicalOperations(number1 int, number2 int, operation string) {
+	switch operation {
 	case "+":
 		conclusion(number1 + number2)
-	case "-":	
+	case "-":
 		conclusion(number1 - number2)
 	case "*":
 		conclusion(number1 * number2)
 	case "/":
-		if number2 == 0{
+		if number2 == 0 {
 			conclusion("Division by zero")
-		}else{
+		} else {
 			conclusion(number1 / number2)
 		}
 	default:
-		fmt.Println("Invalid operation")
+		conclusion("Invalid operation")
 	}
 }
 
-func main(){
+func main() {
 	var number1, number2 int
-	var theAddition string
+	var operation string
+
 	conclusion("Введите первое число:")
-	if _,err := fmt.Scanln(&number1); err != nil{
+	if _, err := fmt.Scanln(&number1); err != nil {
 		conclusion("Invalid first operand")
 		return
 	}
+
 	conclusion("Введите второе число:")
-	if _,err := fmt.Scanln(&number2); err != nil{
+	if _, err := fmt.Scanln(&number2); err != nil {
 		conclusion("Invalid second operand")
 		return
 	}
+
 	conclusion("Введите символ операции:")
-	if _,err := fmt.Scanln(&theAddition); err != nil{
+	if _, err := fmt.Scanln(&operation); err != nil {
 		conclusion("Invalid operation")
 		return
 	}
-	mathematicalOperations(number1, number2,theAddition)
 
-} 
+	mathematicalOperations(number1, number2, operation)
+}
