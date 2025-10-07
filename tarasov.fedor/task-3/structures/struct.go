f559191264bf5778427a2c169ad5feee478ee9ad
WellC2G <fedorka2010@gmail.com>
2025-10-07 17:26:34 +0300

    [TASK-3] edit num_code type from string to int

--- tarasov.fedor/task-3/structures/struct.go
+++ tarasov.fedor/task-3/structures/struct.go
@@ -9,7 +9,7 @@ type ValCurs struct {
 	Valute []Valute `xml:"Valute"`
 }
 type Valute struct {
-	NumCode  string `xml:"NumCode" json:"num_code"`
+	NumCode  int    `xml:"NumCode" json:"num_code"`
 	CharCode string `xml:"CharCode" json:"char_code"`
 	Value    string `xml:"Value" json:"value"`
 }
