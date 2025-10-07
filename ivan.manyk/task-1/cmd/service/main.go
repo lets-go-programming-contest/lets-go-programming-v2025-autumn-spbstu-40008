687f2e6df2e33f2141be386ac4a4fab6844b2960
я <manykivan23@gmail.com>
2025-10-07 03:02:24 +0300

    [TASK-1] Add fmt.Println

--- ivan.manyk/task-1/cmd/service/main.go
+++ ivan.manyk/task-1/cmd/service/main.go
@@ -7,33 +7,33 @@ func main() {
 	var symbol string
 	_, err := fmt.Scan(&a)
 	if err != nil {
-		fmt.Print("Invalid first operand")
+		fmt.Println("Invalid first operand")
 		return
 	}
 	_, err = fmt.Scan(&b)
 	if err != nil {
-		fmt.Print("Invalid second operand")
+		fmt.Println("Invalid second operand")
 		return
 	}
 	_, err = fmt.Scan(&symbol)
 	if err != nil {
-		fmt.Print("Invalid operation")
+		fmt.Println("Invalid operation")
 		return
 	}
 	switch (symbol) {
 		case "+":
-			fmt.Print(a+b)
+			fmt.Println(a+b)
 		case "-":
-			fmt.Print(a-b)
+			fmt.Println(a-b)
 		case "*":
-			fmt.Print(a*b)
+			fmt.Println(a*b)
 		case "/":
 			if b == 0 {
-				fmt.Print("Division by zero")
+				fmt.Println("Division by zero")
 				return
 			}
-			fmt.Print(a/b)
+			fmt.Println(a/b)
 		default:
-			fmt.Print("Invalid operation")
+			fmt.Println("Invalid operation")
 	}
 }
