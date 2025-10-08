13034fbf643ad496ba642d1d5952a048be605b9b
juliawheatman <julia.wheatman@gmail.com>
2025-10-08 17:24:24 +0300

    [TASK-2-1] Delete print help messages, fix lint problems

--- julia.pshenitsyna/task-2-1/cmd/service/main.go
+++ julia.pshenitsyna/task-2-1/cmd/service/main.go
@@ -9,27 +9,29 @@ func main() {
 		departments, employees, maxT, minT, temperature int
 		sign                                            string
 	)
-	fmt.Println("Введите отделы:")
+	
 	_, err := fmt.Scan(&departments)
 	if err != nil {
 		return
 	}
+
 	for range departments { // считаем по каждому отделу
 
-		fmt.Println("Введите сотрудников:")
 		_, err := fmt.Scan(&employees)
 		if err != nil {
 			return
 		}
+
 		maxT = 10000000000000
 		minT = 0
-		for range employees { //считаем по каждому сторуднику
 
-			fmt.Println("Введите температуру:")
+		for range employees { // считаем по каждому сторуднику
+
 			_, err := fmt.Scanf("\n%s %d", &sign, &temperature)
 			if err != nil {
 				return
 			}
+
 			switch sign {
 			case ">=":
 				minT = max(minT, temperature)
@@ -38,10 +40,12 @@ func main() {
 			default:
 				continue
 			}
+
 			if minT > maxT {
 				fmt.Println(-1)
 				continue
 			}
+
 			fmt.Println(min(minT, maxT))
 		}
 	}
