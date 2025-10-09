97e36f256c3307a917f60a98795b8aa61f3ea9a2
Ulyashkass <hukumka1978@gmail.com>
2025-10-10 02:14:33 +0300

    [TASK-2-1] fix lint

--- kuzminykh.ulyana/task-2-1/cmd/service/main.go
+++ kuzminykh.ulyana/task-2-1/cmd/service/main.go
@@ -6,37 +6,36 @@ import (
 
 func main() {
 	var (
-		N, K, curTemp, minTemp, maxTemp int
-		sign                            string
+		dep, emp, curTemp, minTemp, maxTemp int
+		sign                                string
 	)
 
-	_, err := fmt.Scan(&N)
+	_, err := fmt.Scan(&dep)
 	if err != nil {
 		fmt.Println("Invalid value")
 	}
 
-	for range N {
-		_, err = fmt.Scan(&K)
+	for range dep {
+		_, err = fmt.Scan(&emp)
 		if err != nil {
 			fmt.Println("Invalid value")
 		}
+
 		maxTemp = 30
 		minTemp = 15
 
-		for range K {
-			fmt.Scan(&sign, &curTemp)
-			if curTemp <= 30 && curTemp >= 15 {
-				switch sign {
-				case "<=":
-					maxTemp = min(maxTemp, curTemp)
-				case ">=":
-					minTemp = max(minTemp, curTemp)
-				default:
-					fmt.Println("Invalid operation")
-				}
-			} else {
-				fmt.Println(-1)
+		for range emp {
+			_, err = fmt.Scan(&sign, &curTemp)
+
+			switch sign {
+			case "<=":
+				maxTemp = min(maxTemp, curTemp)
+			case ">=":
+				minTemp = max(minTemp, curTemp)
+			default:
+				fmt.Println("Invalid operation")
 			}
+
 			if maxTemp < minTemp {
 				fmt.Println(-1)
 			} else {
