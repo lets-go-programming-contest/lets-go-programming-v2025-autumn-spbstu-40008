a5ffc25adbe26fee5b3c22c658de226c334f03c4
mordw1n <lolpovar0@gmail.com>
2025-10-10 02:18:17 +0300

    [TASK-2-2] init of vars + process err

--- mikhail.spiridonov/task-2-2/cmd/service/main.go
+++ mikhail.spiridonov/task-2-2/cmd/service/main.go
@@ -1,6 +1,28 @@
 package main
 
-import "fmt"
 import "github.com/mordw1n/task-2-2/internal/heap"
 
+func main()  {
+	var countOfDishes int
+	var a_i           int
+	var num_k         int
 
+	if _, err := fmt.Scan(&countOfDishes); err != nil {
+		fmt.Printf("Bad input for dishes: %v\n", err)
+
+		return
+	}
+
+		if _, err := fmt.Scan(&a_i); err != nil {
+		fmt.Printf("Bad input for sequence: %v\n", err)
+
+		return
+	}
+
+		if _, err := fmt.Scan(&num_k); err != nil {
+		fmt.Printf("Bad input for dish preference: %v\n", err)
+
+		return
+	}
+
+im}
