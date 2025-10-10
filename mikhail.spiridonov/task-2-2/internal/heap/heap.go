51225bff0ce0914d6d073a87eeec03823a0691d8
mordw1n <lolpovar0@gmail.com>
2025-10-10 03:06:02 +0300

    [TASK-2-2] init func's in heap

--- mikhail.spiridonov/task-2-2/internal/heap/heap.go
+++ mikhail.spiridonov/task-2-2/internal/heap/heap.go
@@ -3,4 +3,24 @@ package heap
 import "container/heap"
 import "fmt"
 
+type Heap []int
 
+func (object Heap) Len() int {
+
+} 
+
+func (object Heap) Less() bool {
+
+}
+
+func (object Heap) Swap() int {
+
+}
+
+func (object Heap) Push() int {
+
+}
+
+func (object Heap) Pop()  {  //ret int || any
+
+}
