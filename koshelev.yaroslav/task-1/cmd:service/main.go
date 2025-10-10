231f68602d0f75541d8128ee6b6e1519672fb9a0
senku812 <xaomi29122017@gmail.com>
2025-10-10 15:13:25 +0300

    [TASK-1] Initial commit

---
+++ koshelev.yaroslav/task-1/cmd:service/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fstNum int
+	var secNum int
+	var op string
+
+	fmt.Println("Enter the first number: ")
+	_, err1 := fmt.Scan(&fstNum)
+	if err1 != nil {
+		fmt.Println("Invalid first operand:", err1)
+		return
+	}
+
+	fmt.Println("Enter the second number:")
+	_, err2 := fmt.Scan(&secNum)
+	if err2 != nil {
+		fmt.Println("Invalid second operand:", err2)
+		return
+	}
+
+	_, _ = fmt.Scan(&op)
+
+	if !rightOp(op) {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	result := calculate(fstNum, secNum, op)
+	fmt.Println("Result: ", result)
+}
+
+func rightOp(op string) bool {
+	switch op {
+	case "+", "-", "*", "/":
+		return true
+	default:
+		return false
+	}
+}
+
+func calculate(a int, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return 0
+		}
+		return a / b
+	default:
+		return 0
+	}
+}
---
+++ koshelev.yaroslav/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/senku812/task-1
+
+go 1.25.1
