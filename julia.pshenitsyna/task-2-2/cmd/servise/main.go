31f655ac643b02c624d40a2bfaf995b89aca32c9
juliawheatman <julia.wheatman@gmail.com>
2025-10-15 01:56:42 +0300

    [TASK-2-2] go mod problems, lint

--- julia.pshenitsyna/task-2-2/cmd/servise/main.go
+++ julia.pshenitsyna/task-2-2/cmd/servise/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "container/heap"
-  "fmt"
+    "container/heap"
+    "fmt"
 )
 
 type IntHeap []int
@@ -12,54 +12,56 @@ func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
 func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
 func (h *IntHeap) Push(x interface{}) {
-  *h = append(*h, x.(int))
+    *h = append(*h, x.(int))
 }
 
 func (h *IntHeap) Pop() interface{} {
-  old := *h
-  n := len(old)
-  x := old[n-1]
-  *h = old[0 : n-1]
-  return x
+    old := *h
+    n := len(old)
+    x := old[n-1]
+    *h = old[0 : n-1]
+    return x
+
 }
 
-
-
 func main(){
-  rating := &IntHeap{}
-  heap.Init(rating)
+    rating := &IntHeap{}
+    heap.Init(rating)
 
-  var (
-    nDishes int
-    nRating int
-    temp int
-  )
+    var (
+        nDishes int
+        nRating int
+        temp int
+	)
 
-  _, err := fmt.Scan(&nDishes)
-  if err != nil {
-    fmt.Println("Invalid number")
-    return
-  }
-
-  for range nDishes{
-      _, err = fmt.Scan(&temp)
-      if err != nil{
+	_, err := fmt.Scan(&nDishes)
+	if err != nil {
         fmt.Println("Invalid number")
         return
-      }
 
-      heap.Push(rating, temp)
-  }
-  
-  _, err = fmt.Scan(&nRating)
-  if err != nil {
-    fmt.Println("Invalid number")
-    return
-  }
+	}
 
-  for i := 1; i < nRating; i++ {
-    heap.Pop(rating)
-  }
+	for range nDishes{
+		_, err = fmt.Scan(&temp)
+		if err != nil{
+			fmt.Println("Invalid number")
+			return
 
-  fmt.Println(heap.Pop(rating))
+		}
+
+		heap.Push(rating, temp)
+	}
+	
+	_, err = fmt.Scan(&nRating)
+	if err != nil {
+		fmt.Println("Invalid number")
+		return
+
+	}
+
+	for i := 1; i < nRating; i++ {
+		heap.Pop(rating)
+	}
+
+	fmt.Println(heap.Pop(rating))
 }
