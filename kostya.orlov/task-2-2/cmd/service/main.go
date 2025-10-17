94a8cb69de347355f66067b283038cc7303ff5ec
OrlovKostya <orlov707kostya@gmail.com>
2025-10-17 20:15:49 +0300

    [TASK-2-2] Initial commit

---
+++ kostya.orlov/task-2-2/cmd/service/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"os"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(elem interface{}) {
+	*h = append(*h, elem.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	elem := old[n-1]
+	*h = old[0 : n-1]
+	return elem
+}
+
+func main() {
+	var (
+		countDishes, priority, result int
+	)
+
+	dishes := &IntHeap{}
+	heap.Init(dishes)
+
+	if _, err := fmt.Scan(&countDishes); err != nil {
+		fmt.Println("Error input for count dishes")
+		os.Exit(0)
+	}
+
+	for range countDishes {
+		var valDish int
+
+		if _, err := fmt.Scan(&valDish); err != nil {
+			fmt.Println("Error input for value dish")
+			os.Exit(0)
+		}
+
+		heap.Push(dishes, valDish)
+	}
+
+	if _, err := fmt.Scan(&priority); err != nil {
+		fmt.Println("Error input for dish's priority")
+		os.Exit(0)
+	}
+
+	priority = dishes.Len() - priority + 1
+
+	for range priority {
+		result = heap.Pop(dishes).(int)
+	}
+
+	fmt.Println(result)
+}
