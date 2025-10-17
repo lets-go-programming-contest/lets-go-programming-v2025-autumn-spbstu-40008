47cda430625ef692c3ec7200da0030e5053d7177
Ulyashkass <hukumka1978@gmail.com>
2025-10-17 21:12:46 +0300

    [TASK-2-2] fix pointer

--- kuzminykh.ulyana/task-2-2/cmd/service/main.go
+++ kuzminykh.ulyana/task-2-2/cmd/service/main.go
@@ -7,16 +7,16 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] > h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Pop() interface{} {
@@ -27,14 +27,13 @@ func (h *IntHeap) Pop() interface{} {
 
 	return x
 }
+
 func (h *IntHeap) Push(x interface{}) {
 	*h = append(*h, x.(int))
 }
 
 func main() {
-	var (
-		countFood, currFood, numFood int
-	)
+	var countFood, currFood, numFood int
 
 	foodHeap := &IntHeap{}
 	heap.Init(foodHeap)
