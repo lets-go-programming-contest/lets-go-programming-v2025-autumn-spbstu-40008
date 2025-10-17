9d026abcb65d5e344c9caf4eb0484233ab72d19d
OrlovKostya <orlov707kostya@gmail.com>
2025-10-17 20:32:44 +0300

    [TASK-2-2] change if for Less and delete crutch in line 57

--- kostya.orlov/task-2-2/cmd/service/main.go
+++ kostya.orlov/task-2-2/cmd/service/main.go
@@ -9,7 +9,7 @@ import (
 type IntHeap []int
 
 func (h *IntHeap) Len() int           { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
 func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(elem interface{}) {
@@ -53,8 +53,6 @@ func main() {
 		fmt.Println("Error input for dish's priority")
 		os.Exit(0)
 	}
-
-	priority = dishes.Len() - priority + 1
 
 	for range priority {
 		if val, ok := heap.Pop(dishes).(int); ok {
