0597f8df16ac63ea171f681e6cafd7da4fffb747
Dana Esko <danaesko12@gmail.com>
2025-10-17 19:58:54 +0300

    [TASK-2-2] Initial commit

---
+++ esko.dana/task-2-2/cmd/service/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntHeap) Push(x any) {
+	if val, ok := x.(int); ok {
+		*h = append(*h, val)
+	}
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
+
+func main() {
+	var dishNum, k, temp int
+
+	rating := &IntHeap{}
+	heap.Init(rating)
+
+	_, err := fmt.Scan(&dishNum)
+	if err != nil {
+		fmt.Println("Invalid number")
+
+		return
+	}
+
+	for range dishNum {
+		_, err = fmt.Scan(&temp)
+		if err != nil {
+			fmt.Println("Invalid number")
+
+			return
+		}
+
+		heap.Push(rating, temp)
+	}
+
+	_, err = fmt.Scan(&k)
+	if err != nil {
+		fmt.Println("Invalid number")
+
+		return
+	}
+
+	for i := 0; i < k-1; i++ {
+		heap.Pop(rating)
+	}
+
+	fmt.Println(heap.Pop(rating))
+}
