2cce2ce420aad246a9bd0c505b7ef260a110a08d
OrlovKostya <orlov707kostya@gmail.com>
2025-10-17 17:06:27 +0300

    [TASK-2-1] change all in this file...

--- kostya.orlov/task-2-1/cmd/service/main.go
+++ kostya.orlov/task-2-1/cmd/service/main.go
@@ -1,72 +1,48 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
-var reader = bufio.NewReader(os.Stdin)
-
-func readLine() (string, error) {
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(line), nil
-}
-
 func main() {
-	var n int
+
+	var (
+		n int
+	)
 
 	if _, err := fmt.Scan(&n); err != nil {
+		fmt.Println("Error input N")
 		os.Exit(0)
 	}
 
 	for i := 0; i < n; i++ {
-		var k int
+		var (
+			k, temp  int
+			upper    = 30
+			lower    = 15
+			operator string
+		)
 
 		if _, err := fmt.Scan(&k); err != nil {
+			fmt.Println("Error input K")
 			os.Exit(0)
 		}
 
-		lower := 15
-		upper := 30
-
 		for j := 0; j < k; j++ {
-			temp, err := readLine()
-
-			if err != nil || temp == "" {
+			if _, err := fmt.Scan(&operator, &temp); err != nil {
+				fmt.Println("Error input temperature")
 				os.Exit(0)
 			}
 
-			parts := strings.Fields(temp)
-			if len(parts) != 2 {
-				upper = -1
-				lower = 0
-			} else {
-				operator := parts[0]
-				degreeStr := parts[1]
-
-				degree, err := strconv.Atoi(degreeStr)
-				if err != nil {
-					os.Exit(0)
-				}
-
-				switch operator {
-				case "<=":
-					if degree < upper {
-						upper = degree
-					}
-				case ">=":
-					if degree > lower {
-						lower = degree
-					}
-				default:
-					continue
-				}
+			switch operator {
+			case "<=":
+				upper = min(upper, temp)
+			case ">=":
+				lower = max(lower, temp)
+			default:
+				fmt.Println("Invalid operator")
+				os.Exit(0)
 			}
 
 			if lower > upper {
@@ -76,4 +52,5 @@ func main() {
 			}
 		}
 	}
+
 }
