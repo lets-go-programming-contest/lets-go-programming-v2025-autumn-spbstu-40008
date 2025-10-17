2651bf7d023c0fb43368a3e571adbdd4663b8dd5
Marfina <almarfina@gmail.com>
2025-10-17 23:13:20 +0300

    [TASK-2-2] linter, again.

--- alisa.marfina/task-2-2/cmd/service/main.go
+++ alisa.marfina/task-2-2/cmd/service/main.go
@@ -10,9 +10,11 @@ type IntHeap []int
 func (h IntHeap) Len() int {
 	return len(h)
 }
+
 func (h IntHeap) Less(i, j int) bool {
 	return h[i] < h[j]
 }
+
 func (h IntHeap) Swap(i, j int) {
 	h[i], h[j] = h[j], h[i]
 }
