fb587fd2a9d5e0605c5958ee0979abf55db4e25a
Vlad <vldkudryavtsev@gmail.com>
2025-10-18 12:44:04 +0300

    [TASK-2-2] Fixed gofmt

--- kudryavtsev.vladislav/task-2-2/cmd/service/main.go
+++ kudryavtsev.vladislav/task-2-2/cmd/service/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
- "container/heap"
- "fmt"
+	"container/heap"
+	"fmt"
 )
 
 type IntHeap []int
@@ -12,62 +12,62 @@ func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
 func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x any) {
- val, ok := x.(int)
- if !ok {
-  return
- }
+	val, ok := x.(int)
+	if !ok {
+		return
+	}
 
- *h = append(*h, val)
+	*h = append(*h, val)
 }
 
 func (h *IntHeap) Pop() any {
- old := *h
- n := len(old)
- x := old[n-1]
- *h = old[:n-1]
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
 
- return x
+	return x
 }
 
 func main() {
- var (
-  numElements int
-  value       int
-  kthIndex    int
-  minHeap     = &IntHeap{}
-  sortedHeap  []int
- )
+	var (
+		numElements int
+		value       int
+		kthIndex    int
+		minHeap     = &IntHeap{}
+		sortedHeap  []int
+	)
 
- _, err := fmt.Scan(&numElements)
- if err != nil {
-  return
- }
+	_, err := fmt.Scan(&numElements)
+	if err != nil {
+		return
+	}
 
- for i := 0; i < numElements; i++ {
-  _, err = fmt.Scan(&value)
-  if err != nil {
-   return
-  }
+	for i := 0; i < numElements; i++ {
+		_, err = fmt.Scan(&value)
+		if err != nil {
+			return
+		}
 
-  heap.Push(minHeap, value)
- }
+		heap.Push(minHeap, value)
+	}
 
- _, err = fmt.Scan(&kthIndex)
- if err != nil {
-  return
- }
+	_, err = fmt.Scan(&kthIndex)
+	if err != nil {
+		return
+	}
 
- for minHeap.Len() > 0 {
-  item := heap.Pop(minHeap)
+	for minHeap.Len() > 0 {
+		item := heap.Pop(minHeap)
 
-  val, ok := item.(int)
-  
-  if !ok {
-   return
-  }
+		val, ok := item.(int)
 
-  sortedHeap = append(sortedHeap, val)
- }
+		if !ok {
+			return
+		}
 
- fmt.Println(sortedHeap[len(sortedHeap)-kthIndex])
+		sortedHeap = append(sortedHeap, val)
+	}
+
+	fmt.Println(sortedHeap[len(sortedHeap)-kthIndex])
 }
