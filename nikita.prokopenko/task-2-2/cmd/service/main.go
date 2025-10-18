c8338cb84909d9d30b5fefa8651bfde132e3ba2f
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-10-18 15:30:52 +0300

    [TASK-2-2] Initial commit

---
+++ nikita.prokopenko/task-2-2/cmd/service/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int            { return len(h) }
+func (h IntHeap) Less(i, j int) bool  { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func main() {
+	var n, k int
+	fmt.Scan(&n)
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+	fmt.Scan(&k)
+	h := &IntHeap{}
+	heap.Init(h)
+	for _, v := range a {
+		heap.Push(h, v)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	fmt.Println((*h)[0])
+}
