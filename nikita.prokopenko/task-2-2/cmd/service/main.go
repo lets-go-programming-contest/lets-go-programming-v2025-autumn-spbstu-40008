a10831e886065b049e4807a1dcf1b6a6830439d2
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-10-18 15:47:02 +0300

    [TASK-2-2] Fix test2

--- nikita.prokopenko/task-2-2/cmd/service/main.go
+++ nikita.prokopenko/task-2-2/cmd/service/main.go
@@ -7,9 +7,9 @@ import (
 
 type IntHeap []int
 
-func (h *IntHeap) Len() int            { return len(*h) }
-func (h *IntHeap) Less(i, j int) bool  { return (*h)[i] < (*h)[j] }
-func (h *IntHeap) Swap(i, j int)       { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+func (h *IntHeap) Len() int { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 func (h *IntHeap) Push(x interface{}) {
 	v, ok := x.(int)
 	if !ok {
@@ -17,42 +17,48 @@ func (h *IntHeap) Push(x interface{}) {
 	}
 	*h = append(*h, v)
 }
-
 func (h *IntHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
-
 	*h = old[:n-1]
-
 	return x
 }
 
 func main() {
-	var count, kth int
-	if _, err := fmt.Scan(&count); err != nil {
+	var count int
+	_, err := fmt.Scan(&count)
+	if err != nil {
 		return
 	}
-	arr := make([]int, count)
-	for i := range arr {
-		if _, err := fmt.Scan(&arr[i]); err != nil {
+
+	items := make([]int, count)
+	for i := 0; i < count; i++ {
+		_, err = fmt.Scan(&items[i])
+		if err != nil {
 			return
 		}
 	}
-	if _, err := fmt.Scan(&kth); err != nil {
+
+	var kth int
+	_, err = fmt.Scan(&kth)
+	if err != nil {
 		return
 	}
+
 	minHeap := &IntHeap{}
 	heap.Init(minHeap)
-	for _, val := range arr {
-		heap.Push(minHeap, val)
 
+	for _, val := range items {
+		heap.Push(minHeap, val)
 		if minHeap.Len() > kth {
 			heap.Pop(minHeap)
 		}
 	}
 
-	top := (*minHeap)[0]
+	if minHeap.Len() == 0 {
+		return
+	}
 
-	fmt.Println(top)
+	fmt.Println((*minHeap)[0])
 }
