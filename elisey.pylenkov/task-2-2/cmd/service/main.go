84f6b0cc0da8590f2efea34d31fd4dc2a29f24bc
foriinrage <eliseypylenka@gmail.com>
2025-10-18 16:40:45 +0300

    [TASK-2-2] Linter fix

--- elisey.pylenkov/task-2-2/cmd/service/main.go
+++ elisey.pylenkov/task-2-2/cmd/service/main.go
@@ -7,15 +7,22 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Push(x any)        { *h = append(*h, x.(int)) }
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+func (h *IntHeap) Push(x any) {
+	v, ok := x.(int)
+	if !ok {
+		return
+	}
+	*h = append(*h, v)
+}
 func (h *IntHeap) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
@@ -30,7 +37,7 @@ func main() {
 	}
 
 	for range numDishes {
-		if _, err := fmt.Scan(&dishRating); err != nil {
+		if _, err := fmt.Scan(&dishRating); err == nil {
 			heap.Push(sortHeap, dishRating)
 		}
 	}
