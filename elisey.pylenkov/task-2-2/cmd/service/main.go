0600c28803db12548267b6f78ddaf04df37fd778
foriinrage <eliseypylenka@gmail.com>
2025-10-18 16:58:11 +0300

    [TASK-2-2] Formated code #2

--- elisey.pylenkov/task-2-2/cmd/service/main.go
+++ elisey.pylenkov/task-2-2/cmd/service/main.go
@@ -11,11 +11,9 @@ func (h *IntHeap) Len() int           { return len(*h) }
 func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
 func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 func (h *IntHeap) Push(x any) {
-	v, ok := x.(int)
-	if !ok {
-		return
+	if v, ok := x.(int); ok {
+		*h = append(*h, v)
 	}
-	*h = append(*h, v)
 }
 
 func (h *IntHeap) Pop() any {
