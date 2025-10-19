93bfbec5c3da963cd53f3e6be11bb6924f3eeb0d
Andrey <jdh994lr@gmail.com>
2025-10-20 01:18:08 +0300

    [TASK-2-1] Initial commit (#70)
    * [TASK-2-1] Initial commit
    
    * [TASK-2-1] fix variable name
    
    * [TASK-2-1] format fix

---
+++ likhodievskii.andrey/task-2-1/cmd/service/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	errorValue          = -1
+	defaultMinTempValue = 15
+	defaultMaxTempValue = 30
+)
+
+func main() {
+	var (
+		departmentCount, countEmployess, temp int
+		wishTempSign                          string
+	)
+
+	if _, err := fmt.Scan(&departmentCount); err != nil {
+		fmt.Printf("Bad input for departments: %v\n", err)
+
+		return
+	}
+
+	for range departmentCount {
+		if _, err := fmt.Scan(&countEmployess); err != nil {
+			fmt.Printf("Bad input for employess: %v\n", err)
+
+			return
+		}
+
+		var (
+			departmentMinTempValue = defaultMinTempValue
+			departmentMaxTempValue = defaultMaxTempValue
+		)
+
+		for range countEmployess {
+			if _, err := fmt.Scan(&wishTempSign, &temp); err != nil {
+				fmt.Printf("Bad input: %v\n", err)
+
+				return
+			}
+
+			switch wishTempSign {
+			case "<=":
+				departmentMaxTempValue = min(departmentMaxTempValue, temp)
+			case ">=":
+				departmentMinTempValue = max(departmentMinTempValue, temp)
+			default:
+				fmt.Printf("Invalid sign: %s\n", wishTempSign)
+
+				return
+			}
+
+			if departmentMaxTempValue < departmentMinTempValue {
+				fmt.Println(errorValue)
+			} else {
+				fmt.Println(departmentMinTempValue)
+			}
+		}
+	}
+}
