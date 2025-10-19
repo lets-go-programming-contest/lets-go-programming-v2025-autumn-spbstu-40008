8abb376c72da84ac5dc9ebf442462e0badca4149
Илья Ерофеевский <ilyaerofick@gmail.com>
2025-10-20 00:40:14 +0300

    [TASK-2-1] (#44)
    * [TASK-2-1]
    
    * [TASK-2-1] Fix lint go code
    
    * [TASK-2-1] Second try fix lint go code
    
    * [TASK-2-1] Fixed mistakes and variable
    
    * [TASK-2-1] Another fixed mistakes and variable
    
    * [TASK-2-1] Fixed mistakes
    
    * [TASK-2-1] Fixed mistakes

---
+++ ilya.erofeevsky/task-2-1/cmd/service/main.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+const (
+	MinTemp  = 15
+	MaxTemp  = 30
+	ErrorVal = -1
+)
+
+func main() {
+	var (
+		coutDepart, coutWorkers, temp int
+		symbols                       string
+	)
+
+	_, err := fmt.Scan(&coutDepart)
+	if err != nil {
+		fmt.Println("Error reading count of departaments")
+
+		return
+	}
+
+	for range coutDepart {
+		_, err = fmt.Scan(&coutWorkers)
+		if err != nil {
+			fmt.Println("Incorrect quantity workers")
+
+			return
+		}
+
+		currentMax := MaxTemp
+		currentMin := MinTemp
+
+		for range coutWorkers {
+			_, err = fmt.Scan(&symbols)
+			if err != nil {
+				fmt.Println("Incorrect symbol")
+
+				return
+			}
+
+			_, err = fmt.Scan(&temp)
+			if err != nil {
+				fmt.Println("Incorrect temperature")
+
+				return
+			}
+
+			switch symbols {
+			case "<=":
+				currentMax = min(currentMax, temp)
+			case ">=":
+				currentMin = max(currentMin, temp)
+			default:
+				fmt.Println("Warning: Unknow symbol")
+
+				continue
+			}
+
+			if currentMax < currentMin {
+				fmt.Println(ErrorVal)
+
+				continue
+			}
+
+			fmt.Println(currentMin)
+		}
+	}
+}
