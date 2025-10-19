d8590f06a01fdf1af7d636e0b68a86548a7491ba
temaPop1e <popov.artoym7@gmail.com>
2025-10-20 01:47:55 +0300

    [TASK2-1] intial commit

---
+++ popov.artem/task-2-1/cmd/service/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readInt(reader *bufio.Reader) (int, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, fmt.Errorf("ошибка чтения: %w", err)
+	}
+	line = strings.TrimSpace(line)
+	num, err := strconv.Atoi(line)
+	if err != nil {
+		return 0, fmt.Errorf("ошибка конвертации: %w", err)
+	}
+	return num, nil
+}
+
+func readLine(reader *bufio.Reader) (string, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("ошибка чтения: %w", err)
+	}
+	return strings.TrimSpace(line), nil
+}
+
+func updateRange(input string, minTemp, maxTemp *int) int {
+	const minLen = 2
+	if len(input) < minLen {
+		return 0
+	}
+
+	prefix := input[:2]
+	if strings.HasPrefix(prefix, ">=") {
+		numStr := strings.TrimSpace(input[2:])
+		if numStr != "" && numStr[0] == ' ' {
+			numStr = strings.TrimSpace(numStr[1:])
+		}
+		value, err := strconv.Atoi(numStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if value > *minTemp {
+			*minTemp = value
+		}
+	} else if strings.HasPrefix(prefix, "<=") {
+		numStr := strings.TrimSpace(input[2:])
+		if numStr != "" && numStr[0] == ' ' {
+			numStr = strings.TrimSpace(numStr[1:])
+		}
+		value, err := strconv.Atoi(numStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if value < *maxTemp {
+			*maxTemp = value
+		}
+	}
+
+	if *minTemp <= *maxTemp {
+		return *minTemp
+	}
+	return -1
+}
+
+func main() {
+	scanner := bufio.NewReader(os.Stdin)
+
+	testCount, err := readInt(scanner)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < testCount; i++ {
+		conditionCount, err := readInt(scanner)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		min, max := 15, 30
+
+		for j := 0; j < conditionCount; j++ {
+			line, err := readLine(scanner)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			result := updateRange(line, &min, &max)
+			fmt.Println(result)
+		}
+	}
+}
