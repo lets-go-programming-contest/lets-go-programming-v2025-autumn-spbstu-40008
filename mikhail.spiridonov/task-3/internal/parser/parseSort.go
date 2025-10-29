d7962a11ac01efd4d6fc4759c949dfacc2d06a04
mordw1n <lolpovar0@gmail.com>
2025-10-29 20:41:20 +0300

    [TASK-3] del unused pack in parseSort (att2)

--- mikhail.spiridonov/task-3/internal/parser/parseSort.go
+++ mikhail.spiridonov/task-3/internal/parser/parseSort.go
@@ -5,7 +5,6 @@ import (
 	"sort"
 
 	"github.com/mordw1n/task-3/internal/jsonpack"
-	"github.com/mordw1n/task-3/internal/valute"
 	"github.com/mordw1n/task-3/internal/xmlpack"
 )
 
@@ -16,11 +15,11 @@ func ParseAndSortXML(inputFile, outputFile string) error {
 	}
 
 	currencies := valCurs.Valutes
-	
+
 	sort.Slice(currencies, func(first, second int) bool {
 		return currencies[first].Value > currencies[second].Value
 	})
-	
+
 	for index, currency := range currencies {
 		if currency.NumCode == 0 {
 			return fmt.Errorf("Invalid num code at %d", index)
