f368147b9e4798dfb981a71bb42061c28a97e6fa
mordw1n <lolpovar0@gmail.com>
2025-10-29 19:58:59 +0300

    [TASK-3] Fix parseSort.go, add err handling (lint fix)

--- mikhail.spiridonov/task-3/internal/parser/parseSort.go
+++ mikhail.spiridonov/task-3/internal/parser/parseSort.go
@@ -2,7 +2,7 @@ package parser
 
 import (
 	"sort"
-	
+
 	"github.com/mordw1n/task-3/internal/jsonpack"
 	"github.com/mordw1n/task-3/internal/xmlpack"
 )
@@ -11,9 +11,28 @@ func ParseAndSortXML(inputFile, outputFile string) {
 	valCurs := xmlpack.ReadFile(inputFile)
 	currencies := valCurs.Valutes
 	
-	sort.Slice(currencies, func(i, j int) bool {
-		return currencies[i].Value > currencies[j].Value
+	sort.Slice(currencies, func(first, second int) bool {
+		return currencies[first].Value > currencies[second].Value
 	})
 	
-	jsonpack.WriteInFile(outputFile, currencies)
+	for index, currency := range currencies {
+		if currency.NumCode == 0 {
+			return fmt.Errorf("Invalid num code")
+		}
+		if currency.CharCode == "" {
+			return fmt.Errorf("Empty char code")
+		}
+		if currency.Value <= 0 {
+			return fmt.Errorf("Bad value of valute")
+		}
+		if index > 0 && currencies[index-1].Value < currency.Value {
+			return fmt.Errorf("Incorrect sort")
+		}
+	}
+
+	if err := jsonpack.WriteInFile(outputFile, currencies); err != nil {
+		return fmt.Errorf("Write in JSON: %w", err)
+	}
+
+	return nil
 }
