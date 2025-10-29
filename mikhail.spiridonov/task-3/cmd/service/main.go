b8e9b908c7a4a8e7ad8413cff0c3573402528422
mordw1n <lolpovar0@gmail.com>
2025-10-29 17:31:39 +0300

    [TASK-3] restore main

--- mikhail.spiridonov/task-3/cmd/service/main.go
+++ mikhail.spiridonov/task-3/cmd/service/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"flag"
+	"os"
 	"github.com/mordw1n/task-3/config"
 	"github.com/mordw1n/task-3/parser"
 )
 
 func main() {
-	inputFile := flag.String("input", "in/currencies.xml", "path to input XML file")
-	outputFile := flag.String("output", "out/converted.json", "path to output JSON file")
+	configPath := flag.String("config", "config.yaml", "path to config YAML file")
 	flag.Parse()
 	
-	parser.ParseAndSortXML(*inputFile, *outputFile)
+	cfg := config.LoadFile(*configPath)
+	parser.ParseAndSortXML(cfg.InputFile, cfg.OutputFile)
 }
