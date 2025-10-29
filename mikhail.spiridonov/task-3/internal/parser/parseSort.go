e107e12812d83f16505f20cc864b59b8c6fc2f8d
mordw1n <lolpovar0@gmail.com>
2025-10-29 23:16:07 +0300

    [TASK-3] minor fix in parser (add lib)

--- mikhail.spiridonov/task-3/internal/parser/parseSort.go
+++ mikhail.spiridonov/task-3/internal/parser/parseSort.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"errors"
 	"fmt"
 	"sort"
 
@@ -18,7 +19,7 @@ func ParseAndSortXML(inputFile, outputFile string) error {
 	currencies := valCurs.Valutes
 
 	validCurrencies := make([]valute.StructOfXMLandJSON, 0, len(currencies))
-	
+
 	for _, currency := range currencies {
 		if currency.CharCode != "" {
 			validCurrencies = append(validCurrencies, currency)
