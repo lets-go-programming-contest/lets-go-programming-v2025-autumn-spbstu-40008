cff56a246b9f981bef7f9cd0489377c50fa95dd1
mordw1n <lolpovar0@gmail.com>
2025-10-29 19:08:05 +0300

    [TASK-3] Lint fix for xmlfile.go

--- mikhail.spiridonov/task-3/internal/xmlpack/xmlfile.go
+++ mikhail.spiridonov/task-3/internal/xmlpack/xmlfile.go
@@ -1,15 +1,21 @@
 package xmlpack
 
 import (
-	"os"
 	"encoding/xml"
+	"fmt"
+	"os"
 
 	"github.com/mordw1n/task-3/internal/valute"
 )
 
 func ReadFile(filePath string) valute.ValuteCurs {
-	data, _ := os.ReadFile(filePath)
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("Read XML %q: %w", path, err)
+	}
 	var valCurs valute.ValuteCurs
-	xml.Unmarshal(data, &valCurs)
-	return valCurs
+	if err := xml.Unmarshal(data, &valCurs); err != nil {
+		return valCurs, fmt.Errorf("Unmarshal %q: %w", path, err)
+	}
+	return valCurs, nil
 }
