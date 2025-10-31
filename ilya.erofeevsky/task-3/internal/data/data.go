669ce86d1e24fc4cad8759f26c34036de5c07af8
Ilya-Er0fick <ilyaerofick@gmail.com>
2025-10-31 23:34:53 +0300

    [TASK-3] Fixed mistakes 9

--- ilya.erofeevsky/task-3/internal/data/data.go
+++ ilya.erofeevsky/task-3/internal/data/data.go
@@ -101,6 +101,7 @@ func CreateAndWriteJSON(filename string, data []structures.Currency) error {
 	if err != nil {
 		return fmt.Errorf("failed to open or create output file %s: %w", filename, err)
 	}
+
 	defer func() {
 		_ = file.Close()
 	}()
