f57958df6a1b74e7197079ffd7a295f44a45a4b4
temaPop1e <popov.artoym7@gmail.com>
2025-11-07 23:03:22 +0300

    [TASK-2-1] code rebuild

--- popov.artem/task-2-1/cmd/service/main.go
+++ popov.artem/task-2-1/cmd/service/main.go
@@ -3,99 +3,44 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func readInt(reader *bufio.Reader) (int, error) {
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, fmt.Errorf("ошибка чтения: %w", err)
-	}
-	line = strings.TrimSpace(line)
-	num, err := strconv.Atoi(line)
-	if err != nil {
-		return 0, fmt.Errorf("ошибка конвертации: %w", err)
-	}
-	return num, nil
-}
-
-func readLine(reader *bufio.Reader) (string, error) {
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		return "", fmt.Errorf("ошибка чтения: %w", err)
-	}
-	return strings.TrimSpace(line), nil
-}
-
-func parseCondition(input string, currentMin, currentMax int) (int, int, error) {
-	if len(input) < 2 {
-		return currentMin, currentMax, nil
-	}
-
-	prefix := input[:2]
-	numStr := strings.TrimSpace(input[2:])
-
-	if prefix == ">=" {
-		value, err := strconv.Atoi(numStr)
-		if err != nil {
-			return currentMin, currentMax, fmt.Errorf("ошибка при парсинге числа в >=: %w", err)
-		}
-		if value > currentMin {
-			currentMin = value
-		}
-	} else if prefix == "<=" {
-		value, err := strconv.Atoi(numStr)
-		if err != nil {
-			return currentMin, currentMax, fmt.Errorf("ошибка при парсинге числа в <=: %w", err)
-		}
-		if value < currentMax {
-			currentMax = value
-		}
-	}
-	return currentMin, currentMax, nil
-}
-
-func checkRange(min, max int) int {
-	if min <= max {
-		return min
-	}
-	return -1
-}
-
 func main() {
-	scanner := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-	testCount, err := readInt(scanner)
-	if err != nil {
-		log.Fatal(err)
-	}
+	scanner.Scan()
+	n, _ := strconv.Atoi(scanner.Text())
 
-	for i := 0; i < testCount; i++ {
-		_ = i
-		conditionCount, err := readInt(scanner)
-		if err != nil {
-			log.Fatal(err)
+	scanner.Scan()
+	k, _ := strconv.Atoi(scanner.Text())
+
+	minReq := 15
+	maxReq := 30
+
+	for i := 0; i < k; i++ {
+		scanner.Scan()
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+		sign := parts[0]
+		temp, _ := strconv.Atoi(parts[1])
+
+		if sign == ">=" {
+			if temp > minReq {
+				minReq = temp
+			}
+		} else if sign == "<=" {
+			if temp < maxReq {
+				maxReq = temp
+			}
 		}
 
-		minVal, maxVal := 15, 30
-
-		for j := 0; j < conditionCount; j++ {
-			_ = j
-			line, err := readLine(scanner)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			minVal, maxVal, err = parseCondition(line, minVal, maxVal)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			result := checkRange(minVal, maxVal)
-			fmt.Println(result)
+		if minReq <= maxReq {
+			fmt.Println(minReq)
+		} else {
+			fmt.Println(-1)
 		}
 	}
 }
