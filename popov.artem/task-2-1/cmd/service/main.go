ee169c1b3d81f4191b48727308d272f390bf008a
temaPop1e <popov.artoym7@gmail.com>
2025-11-07 23:17:36 +0300

    [TASK-2-1] test fix 1

--- popov.artem/task-2-1/cmd/service/main.go
+++ popov.artem/task-2-1/cmd/service/main.go
@@ -12,35 +12,37 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	scanner.Scan()
-	_, _ = strconv.Atoi(scanner.Text())
+	n, _ := strconv.Atoi(scanner.Text())
 
-	scanner.Scan()
-	k, _ := strconv.Atoi(scanner.Text())
-
-	minReq := 15
-	maxReq := 30
-
-	for range k {
+	for range n {
 		scanner.Scan()
-		line := scanner.Text()
-		parts := strings.Split(line, " ")
-		sign := parts[0]
-		temp, _ := strconv.Atoi(parts[1])
+		k, _ := strconv.Atoi(scanner.Text())
 
-		if sign == ">=" {
-			if temp > minReq {
-				minReq = temp
-			}
-		} else if sign == "<=" {
-			if temp < maxReq {
-				maxReq = temp
-			}
-		}
+		minReq := 15
+		maxReq := 30
 
-		if minReq <= maxReq {
-			fmt.Println(minReq)
-		} else {
-			fmt.Println(-1)
+		for range k {
+			scanner.Scan()
+			line := scanner.Text()
+			parts := strings.Split(line, " ")
+			sign := parts[0]
+			temp, _ := strconv.Atoi(parts[1])
+
+			if sign == ">=" {
+				if temp > minReq {
+					minReq = temp
+				}
+			} else if sign == "<=" {
+				if temp < maxReq {
+					maxReq = temp
+				}
+			}
+
+			if minReq <= maxReq {
+				fmt.Println(minReq)
+			} else {
+				fmt.Println(-1)
+			}
 		}
 	}
 }
