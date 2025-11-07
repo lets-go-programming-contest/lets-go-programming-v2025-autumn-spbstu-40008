0588c004b0eff2ced85c18fef0a2f93718f39d76
temaPop1e <popov.artoym7@gmail.com>
2025-11-07 23:36:31 +0300

    [TASK-2-2] linter fix 6

--- popov.artem/task-2-2/cmd/service/main.go
+++ popov.artem/task-2-2/cmd/service/main.go
@@ -16,15 +16,19 @@ func (h *MaxHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
 func (h *MaxHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *MaxHeap) Push(x interface{}) {
-	item := x.(int)
+	item, ok := x.(int)
+	if !ok {
+		return
+	}
+
 	*h = append(*h, item)
 }
 
 func (h *MaxHeap) Pop() interface{} {
 	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
+	numItems := len(old)
+	x := old[numItems-1]
+	*h = old[0 : numItems-1]
 
 	return x
 }
@@ -34,20 +38,20 @@ func main() {
 
 	nLine, _ := scanner.ReadString('\n')
 	nLine = strings.TrimSpace(nLine)
-	n, _ := strconv.Atoi(nLine)
+	numDishes, _ := strconv.Atoi(nLine)
 
 	aLine, _ := scanner.ReadString('\n')
 	aLine = strings.TrimSpace(aLine)
 	parts := strings.Split(aLine, " ")
 
-	nums := make([]int, n)
+	nums := make([]int, numDishes)
 	for i, part := range parts {
 		nums[i], _ = strconv.Atoi(part)
 	}
 
 	kLine, _ := scanner.ReadString('\n')
 	kLine = strings.TrimSpace(kLine)
-	kValue, _ := strconv.Atoi(kLine)
+	kth, _ := strconv.Atoi(kLine)
 
 	maxHeap := &MaxHeap{}
 	heap.Init(maxHeap)
@@ -57,8 +61,14 @@ func main() {
 	}
 
 	var result int
-	for range kValue {
-		result = heap.Pop(maxHeap).(int)
+	for range kth {
+		item := heap.Pop(maxHeap)
+		value, ok := item.(int)
+		if !ok {
+			continue
+		}
+
+		result = value
 	}
 
 	fmt.Println(result)
