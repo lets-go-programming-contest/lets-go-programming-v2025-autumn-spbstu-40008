ec48e8963f0f49dc3a7f55f1aa1d860b4acdc939
mordw1n <lolpovar0@gmail.com>
2025-11-26 19:18:37 +0300

    [TASK-5] implementing handlers (in conv)

--- mikhail.spiridonov/task-5/pkg/conveyer/handlers.go
+++ mikhail.spiridonov/task-5/pkg/conveyer/handlers.go
@@ -1,6 +1,38 @@
 package conveyer
 
-import "context"
+import (
+	"context"
+)
+
+type decorator struct {
+	fn     DecoratorFunc
+	input  chan string
+	output chan string
+}
+
+func (d *decorator) run(ctx context.Context) error {
+	return d.fn(ctx, d.input, d.output)
+}
+
+type multiplexer struct {
+	fn     MultiplexerFunc
+	input  []chan string
+	output chan string
+}
+
+func (m *multiplexer) run(ctx context.Context) error {
+	return m.fn(ctx, m.input, m.output)
+}
+
+type separator struct {
+	fn     SeparatorFunc
+	input  chan string
+	output []chan string
+}
+
+func (s *separator) run(ctx context.Context) error {
+	return s.fn(ctx, s.input, s.output)
+}
 
 type DecoratorFunc func(
     ctx context.Context,
