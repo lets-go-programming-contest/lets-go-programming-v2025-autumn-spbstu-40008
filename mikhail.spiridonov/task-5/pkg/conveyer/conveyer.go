73ee33e0d32fa3b28e3dfb92364356935747f5a6
mordw1n <lolpovar0@gmail.com>
2025-11-26 18:49:12 +0300

    [TASK-5] implement conveyer core methods 2

--- mikhail.spiridonov/task-5/pkg/conveyer/conveyer.go
+++ mikhail.spiridonov/task-5/pkg/conveyer/conveyer.go
@@ -50,3 +50,62 @@ func New(size int) *DefaultConveyer {
         handlers: make([]handler, 0),
     }
 }
+
+func (c *DefaultConveyer) Run(ctx context.Context) error {
+    defer c.close()
+
+    g, gCtx := errgroup.WithContext(ctx)
+
+    for _, h := range c.handlers {
+        handler := h
+        g.Go(func() error {
+            return handler.run(gCtx)
+        })
+    }
+
+    if err := g.Wait(); err != nil {
+        return fmt.Errorf("conveyer stopped: %w", err)
+    }
+
+    return nil
+}
+
+func (c *DefaultConveyer) Send(input string, data string) error {
+    ch, exists := c.channels[input]
+    if !exists {
+        return fmt.Errorf("%w: %s", ErrChanNotFound, input)
+    }
+
+    ch <- data
+    return nil
+}
+
+func (c *DefaultConveyer) Recv(output string) (string, error) {
+    ch, exists := c.channels[output]
+    if !exists {
+        return "", fmt.Errorf("%w: %s", ErrChanNotFound, output)
+    }
+
+    data, ok := <-ch
+    if !ok {
+        return undefined, nil
+    }
+
+    return data, nil
+}
+
+func (c *DefaultConveyer) getOrCreateChannel(name string) chan string {
+    if ch, exists := c.channels[name]; exists {
+        return ch
+    }
+
+    ch := make(chan string, c.size)
+    c.channels[name] = ch
+    return ch
+}
+
+func (c *DefaultConveyer) close() {
+    for _, ch := range c.channels {
+        close(ch)
+    }
+}
