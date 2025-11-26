22194554422ef8c7597289b33ce388ea7f455d53
mordw1n <lolpovar0@gmail.com>
2025-11-26 19:36:58 +0300

    [TASK-5] rename conveyer/handlers.go -> structHandlers.go, realisation of PrefixDecoratorFunc in handlers

--- mikhail.spiridonov/task-5/pkg/handlers/handlers.go
+++ mikhail.spiridonov/task-5/pkg/handlers/handlers.go
@@ -1,11 +1,41 @@
 package handlers
 
 import (
-
+    "context"
+    "errors"
+    "strings"
 )
 
-func PrefixDecoratorFunc() error {
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+    const prefix = "decorated: "
+    const errorSubstring = "no decorator"
+    
+    for {
+        select {
+        case <-ctx.Done():
+            return ctx.Err()
+        case data, ok := <-input:
+            if !ok {
+                return nil
+            }
+            
+            if strings.Contains(data, errorSubstring) {
+                return errors.New("can't be decorated")
+            }
+            
+            if !strings.HasPrefix(data, prefix) {
+                data = prefix + data
+            }
+            
+            select {
+            case output <- data:
+            case <-ctx.Done():
+                return ctx.Err()
+            }
+        }
+    }
 }
+
 
 func SeparatorFunc() error {
 }
