1ac72ed0e3ceec39bb6bed13c4307bc4e7629322
mordw1n <lolpovar0@gmail.com>
2025-11-26 18:42:02 +0300

    [TASK-5] implement conveyer core methods

--- mikhail.spiridonov/task-5/pkg/conveyer/conveyer.go
+++ mikhail.spiridonov/task-5/pkg/conveyer/conveyer.go
@@ -14,13 +14,19 @@ var ErrChanNotFound = errors.New("chan not found")
 
 type Conveyer interface {
     RegisterDecorator(
-
+        fn func(ctx context.Context, input chan string, output chan string) error,
+        input string,
+        output string,
     )
     RegisterMultiplexer(
-
+        fn func(ctx context.Context, inputs []chan string, output chan string) error,
+        inputs []string,
+        output string,
     )
     RegisterSeparator(
-
+        fn func(ctx context.Context, input chan string, outputs []chan string) error,
+        input string,
+        outputs []string,
     )
     Run(ctx context.Context) error
     Send(input string, data string) error
@@ -28,15 +34,19 @@ type Conveyer interface {
 }
 
 type DefaultConveyer struct {
-
+    size     int
+    channels map[string]chan string
+    handlers []handler
 }
 
 type handler interface {
-
+    run(ctx context.Context) error
 }
 
 func New(size int) *DefaultConveyer {
     return &DefaultConveyer{
-
+        size:     size,
+        channels: make(map[string]chan string),
+        handlers: make([]handler, 0),
     }
 }
--- mikhail.spiridonov/task-5/pkg/conveyer/handlers.go
+++ mikhail.spiridonov/task-5/pkg/conveyer/handlers.go
@@ -3,13 +3,19 @@ package conveyer
 import "context"
 
 type DecoratorFunc func(
-
+    ctx context.Context,
+    input chan string,
+    output chan string,
 ) error
 
 type MultiplexerFunc func(
-
+    ctx context.Context,
+    inputs []chan string,
+    output chan string,
 ) error
 
 type SeparatorFunc func(
-
+    ctx context.Context,
+    input chan string,
+    outputs []chan string,
 ) error
