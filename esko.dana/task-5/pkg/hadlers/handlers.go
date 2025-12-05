b1ad20836558b9ae015ba4b6b267d50634fdfe7a
Dana Esko <danaesko12@gmail.com>
2025-12-06 00:10:32 +0300

    [TASK-5] Initial commit

---
+++ esko.dana/task-5/pkg/hadlers/handlers.go
@@ -0,0 +1,133 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var ErrCantBeDecorated = errors.New("can't be decorated")
+var ErrEmptyOutputs = errors.New("empty outputs")
+
+func PrefixDecoratorFunc(
+	ctx context.Context,
+	input, output chan string,
+) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, "no decorator") {
+				return ErrCantBeDecorated
+			}
+
+			if !strings.HasPrefix(data, "decorated: ") {
+				data = "decorated: " + data
+			}
+
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func SeparatorFunc(
+	ctx context.Context,
+	input chan string,
+	outputs []chan string,
+) error {
+	if len(outputs) == 0 {
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case _, ok := <-input:
+				if !ok {
+					return nil
+				}
+			}
+		}
+	}
+
+	idx := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			select {
+			case outputs[idx] <- data:
+			case <-ctx.Done():
+				return nil
+			}
+
+			idx = (idx + 1) % len(outputs)
+		}
+	}
+}
+
+func MultiplexerFunc(
+	ctx context.Context,
+	inputs []chan string,
+	output chan string,
+) error {
+	if len(inputs) == 0 {
+		return nil
+	}
+
+	var wg sync.WaitGroup
+
+	for _, inCh := range inputs {
+		wg.Add(1)
+		go func(ch chan string) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, ok := <-ch:
+					if !ok {
+						return // канал закрыт
+					}
+
+					if strings.Contains(data, "no multiplexer") {
+						continue
+					}
+
+					select {
+					case output <- data:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(inCh)
+	}
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		return nil
+	case <-ctx.Done():
+		wg.Wait()
+		return nil
+	}
+}
