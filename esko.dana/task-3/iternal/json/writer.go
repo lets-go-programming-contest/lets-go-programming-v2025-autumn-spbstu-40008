27305cf59a864263558f12edac7456fd69f82e4a
Dana Esko <danaesko12@gmail.com>
2025-12-05 15:51:11 +0300

    [TASK-3] overdue

---
+++ esko.dana/task-3/iternal/json/writer.go
@@ -0,0 +1,30 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"esko.dana/task-3/iternal/currency"
+)
+
+func Save(currencies []currency.Currency, outputPath string) error {
+	outputDir := filepath.Dir(outputPath)
+	err := os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create output directory '%s': %w", outputDir, err)
+	}
+
+	jsonData, err := json.MarshalIndent(currencies, "", "    ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal results to JSON: %w", err)
+	}
+
+	err = os.WriteFile(outputPath, jsonData, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write results to output file '%s': %w", outputPath, err)
+	}
+
+	return nil
+}
