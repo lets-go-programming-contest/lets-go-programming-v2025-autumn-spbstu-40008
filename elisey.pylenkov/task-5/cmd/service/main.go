a01d4de832d593ddc23d799b299f0ed8862590f5
foriinrage <eliseypylenka@gmail.com>
2025-12-08 02:14:22 +0300

    [TASK-5] Another one test fix

--- elisey.pylenkov/task-5/cmd/service/main.go
+++ elisey.pylenkov/task-5/cmd/service/main.go
@@ -7,81 +7,47 @@ import (
 
 	"task-5/pkg/conveyer"
 	"task-5/pkg/handlers"
-
-	_ "github.com/stretchr/testify/require"
-	_ "golang.org/x/sync/errgroup"
 )
 
 func main() {
 	conv := conveyer.New(10)
 
-	conv.RegisterDecorator(
-		handlers.PrefixDecoratorFunc,
-		"input1",
-		"decorated1",
-	)
-
-	conv.RegisterSeparator(
-		handlers.SeparatorFunc,
-		"decorated1",
-		[]string{"out1", "out2"},
-	)
+	conv.RegisterDecorator(handlers.PrefixDecoratorFunc, "input1", "decorated1")
+	conv.RegisterSeparator(handlers.SeparatorFunc, "decorated1", []string{"out1", "out2"})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	go func() {
 		if err := conv.Run(ctx); err != nil && err != context.Canceled {
-			fmt.Printf("Conveyer error: %v\n", err)
+			fmt.Println("Conveyer error:", err)
 		}
-		fmt.Println("Conveyer stopped")
 	}()
+
+	time.Sleep(50 * time.Millisecond)
+
+	for _, v := range []string{"data1", "data2", "data3"} {
+		if err := conv.Send("input1", v); err != nil {
+			fmt.Println("Send error:", err)
+		}
+	}
+
+	for i := 0; i < 2; i++ {
+		data, err := conv.Recv("out1")
+		if err != nil {
+			fmt.Println("Recv out1 error:", err)
+		} else {
+			fmt.Println("out1:", data)
+		}
+	}
+
+	data, err := conv.Recv("out2")
+	if err != nil {
+		fmt.Println("Recv out2 error:", err)
+	} else {
+		fmt.Println("out2:", data)
+	}
 
 	time.Sleep(100 * time.Millisecond)
-
-	go func() {
-		time.Sleep(50 * time.Millisecond)
-		conv.Send("input1", "test data 1")
-		conv.Send("input1", "test data 2")
-		conv.Send("input1", "test data 3")
-		fmt.Println("All data sent")
-	}()
-
-	done := make(chan bool, 2)
-
-	go func() {
-		for i := 0; i < 2; i++ {
-			data, err := conv.Recv("out1")
-			if err != nil {
-				fmt.Printf("Error reading from out1: %v\n", err)
-			} else {
-				fmt.Printf("out1: %s\n", data)
-			}
-		}
-		done <- true
-	}()
-
-	go func() {
-		for i := 0; i < 1; i++ {
-			data, err := conv.Recv("out2")
-			if err != nil {
-				fmt.Printf("Error reading from out2: %v\n", err)
-			} else {
-				fmt.Printf("out2: %s\n", data)
-			}
-		}
-		done <- true
-	}()
-
-	<-done
-	<-done
-
-	fmt.Println("All data received")
-
-	time.Sleep(500 * time.Millisecond)
-	cancel()
-
-	time.Sleep(200 * time.Millisecond)
-
 	fmt.Println("Program finished")
 }
