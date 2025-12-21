13f1e3d4e4550294c36339c4f815b995422ac263
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2025-12-21 21:02:52 +0300

    [TASK-6] n9

--- nikita.prokopenko/task-6/internal/wifi/WiFiHandle_test.go
+++ nikita.prokopenko/task-6/internal/wifi/WiFiHandle_test.go
@@ -1,48 +1,5 @@
 package wifi_test
 
-import (
-	"errors"
-	"fmt"
-	"net"
+import "testing"
 
-	"github.com/mdlayher/wifi"
-	"github.com/stretchr/testify/mock"
-)
-
-var errTypeAssertion = errors.New("type assertion failed for interface slice")
-
-type MockInterfaceSource struct {
-	mock.Mock
-}
-
-func (m *MockInterfaceSource) Interfaces() ([]*wifi.Interface, error) {
-	args := m.Called()
-
-	if args.Get(0) == nil {
-		if err := args.Error(1); err != nil {
-			return nil, fmt.Errorf("mock error: %w", err)
-		}
-
-		return nil, nil
-	}
-
-	interfaces, ok := args.Get(0).([]*wifi.Interface)
-	if !ok {
-		return nil, errTypeAssertion
-	}
-
-	if err := args.Error(1); err != nil {
-		return interfaces, fmt.Errorf("mock error: %w", err)
-	}
-
-	return interfaces, nil
-}
-
-func createTestInterface(name, macStr string) *wifi.Interface {
-	mac, _ := net.ParseMAC(macStr)
-
-	return &wifi.Interface{
-		Name:         name,
-		HardwareAddr: mac,
-	}
-}
+func TestWiFiHandle_Placeholder(t *testing.T) {}
