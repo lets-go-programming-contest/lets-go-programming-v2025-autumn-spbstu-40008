856c4db61b7163a0dcd95b3c04b279faf929c5e4
LAffey26 <anercontae25@gmail.com>
2025-12-21 06:37:07 +0300

    [TASK-1] init

---
+++ evdokimov.artem/task-1/cmd/service/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1, num2 int
+	var sign string
+
+	if _, err := fmt.Scan(&num1); err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	if _, err := fmt.Scan(&num2); err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	if _, err := fmt.Scan(&sign); err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch sign {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(num1 / num2)
+	default:
+		fmt.Println("Invalid operation")
+	}
+
+}
