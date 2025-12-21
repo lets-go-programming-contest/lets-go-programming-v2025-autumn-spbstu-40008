5effc103ac4acecef6b50daa88ffa9d5b892ff73
LAffey26 <anercontae25@gmail.com>
2025-12-21 07:18:05 +0300

    [TASK-6]

---
+++ evdokimov.artem/task-6/internal/wifi/wifi_mock_test.go
@@ -0,0 +1,21 @@
+package wifi_test
+
+import (
+	"github.com/mdlayher/wifi"
+	"github.com/stretchr/testify/mock"
+)
+
+type mockWiFiHandle struct {
+	mock.Mock
+}
+
+func (mockHandle *mockWiFiHandle) Interfaces() ([]*wifi.Interface, error) {
+	args := mockHandle.Called()
+
+	var list []*wifi.Interface
+	if raw := args.Get(0); raw != nil {
+		list = raw.([]*wifi.Interface) // в тестах мы всегда возвращаем правильный тип
+	}
+
+	return list, args.Error(1)
+}
