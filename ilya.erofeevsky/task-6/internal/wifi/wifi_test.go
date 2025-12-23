68b5b639825cdceac22490532952683b0de9992e
Ilya-Er0fick <ilyaerofick@gmail.com>
2025-12-24 02:15:58 +0300

    [TASK-6]

--- ilya.erofeevsky/task-6/internal/wifi/wifi_test.go
+++ ilya.erofeevsky/task-6/internal/wifi/wifi_test.go
@@ -19,11 +19,9 @@ type MockWiFi struct {
 }
 
 func (m *MockWiFi) Interfaces() ([]*wifipkg.Interface, error) {
-	
 	args := m.Called()
 	res, _ := args.Get(0).([]*wifipkg.Interface)
 	err := args.Error(1)
-
 	if err != nil {
 		return res, fmt.Errorf("mock error: %w", err)
 	}
