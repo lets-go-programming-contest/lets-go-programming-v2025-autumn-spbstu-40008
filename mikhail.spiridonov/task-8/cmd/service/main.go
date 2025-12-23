354a02c69f54ba2ab6f8cbd7d61059ae0fcd7ef8
mordw1n <lolpovar0@gmail.com>
2025-12-23 03:23:20 +0300

    [TASK-8] fix main

--- mikhail.spiridonov/task-8/cmd/service/main.go
+++ mikhail.spiridonov/task-8/cmd/service/main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/mordw1n/task-8/internal/config"
 )
 
 func main() {
-	cfg, err := config.GetConfig()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Loading config: %v\n", err)
-		os.Exit(1)
-	}
-
+	cfg := config.GetConfig()
 	fmt.Print(cfg.Environment, " ", cfg.LogLevel)
 }
