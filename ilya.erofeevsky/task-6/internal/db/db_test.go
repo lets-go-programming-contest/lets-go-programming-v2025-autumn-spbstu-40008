41daa694b33d3d654f182937ba7eb34f18aff21e
Ilya-Er0fick <ilyaerofick@gmail.com>
2025-12-23 21:25:50 +0300

    [TASK-6]

--- ilya.erofeevsky/task-6/internal/db/db_test.go
+++ ilya.erofeevsky/task-6/internal/db/db_test.go
@@ -15,12 +15,12 @@ func TestDB(t *testing.T) {
 		defer sqlDB.Close()
 		service := db.New(sqlDB)
 
-		rows := sqlmock.NewRows([]string{"name"}).AddRow("User1")
+		rows := sqlmock.NewRows([]string{"name"}).AddRow("User1").AddRow("User2")
 		mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
 
 		res, err := service.GetNames()
 		assert.NoError(t, err)
-		assert.Equal(t, []string{"User1"}, res)
+		assert.Equal(t, []string{"User1", "User2"}, res)
 	})
 
 	t.Run("query_error", func(t *testing.T) {
@@ -33,5 +33,20 @@ func TestDB(t *testing.T) {
 		res, err := service.GetNames()
 		assert.Error(t, err)
 		assert.Nil(t, res)
+		assert.Contains(t, err.Error(), "query error")
+	})
+
+	t.Run("scan_error", func(t *testing.T) {
+		sqlDB, mock, _ := sqlmock.New()
+		defer sqlDB.Close()
+		service := db.New(sqlDB)
+
+		rows := sqlmock.NewRows([]string{"name"}).AddRow(123)
+		mock.ExpectQuery("SELECT name FROM users").WillReturnRows(rows)
+
+		res, err := service.GetNames()
+		assert.Error(t, err)
+		assert.Nil(t, res)
+		assert.Contains(t, err.Error(), "scan error")
 	})
 }
