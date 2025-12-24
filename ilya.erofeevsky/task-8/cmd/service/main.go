5d17fa9db32e395e76e1fd4873ac8f96ee152649
Ilya-Er0fick <ilyaerofick@gmail.com>
2025-12-24 03:40:17 +0300

    [TASK-8] Automatic only

---
+++ ilya.erofeevsky/task-8/cmd/service/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"ilya.erofeevsky/task-8/config"
+)
+
+func main() {
+	cfg, err := config.Load(config.ConfigFile)
+	if err != nil {
+		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
+	}
+
+	
+	fmt.Printf("%s %s", cfg.Environment, cfg.LogLevel)
+}
---
+++ ilya.erofeevsky/task-8/config/config_dev.go
@@ -0,0 +1,5 @@
+package config
+
+import _ "embed"
+
+var ConfigFile []byte
