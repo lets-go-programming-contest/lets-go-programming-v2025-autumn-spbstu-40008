f57851b8b7b13cb3ff39f9a333571c422bd973c7
CA21lmFlame <chernov.ar@spbstu.ru>
2026-01-06 00:40:06 +0300

    [TASK-2-1] Initial commit

---
+++ artem.chernov/task-2-1/cmd/service/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var (
+		departments, employees, minT, maxT uint16
+	)
+
+	_, err := fmt.Scan(&departments)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error reading the number of departments: %v\n", err)
+		return
+	}
+
+	for range departments {
+		maxT = 30
+		minT = 15
+
+		_, err = fmt.Scan(&employees)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error readind the number of employees: %v\n", err)
+			return
+		}
+
+		for range employees {
+			var operator string
+			var temperature uint16
+
+			_, err = fmt.Scan(&operator)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error reading the logical operator: %v", err)
+				return
+			}
+
+			_, err = fmt.Scan(&temperature)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error reading temperature: %v", err)
+				return
+			}
+
+			switch operator {
+			case ">=":
+				if temperature > minT {
+					minT = temperature
+				}
+			case "<=":
+				if temperature < maxT {
+					maxT = temperature
+				}
+			default:
+				fmt.Fprintf(os.Stderr, "use only the operators >= or <=")
+			}
+
+			if minT > maxT {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minT)
+			}
+		}
+	}
+}
