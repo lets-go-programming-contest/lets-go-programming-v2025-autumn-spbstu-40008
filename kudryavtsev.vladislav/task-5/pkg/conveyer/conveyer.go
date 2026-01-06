9a970272b137fedf685d8d18a61e9a266fe09243
Vladislav <vldkudryavtsev@gmail.com>
2026-01-06 19:05:27 +0300

    [TASK-5] gofumpted it

--- kudryavtsev.vladislav/task-5/pkg/conveyer/conveyer.go
+++ kudryavtsev.vladislav/task-5/pkg/conveyer/conveyer.go
@@ -19,9 +19,7 @@ type Conveyer struct {
 }
 
 func New(size int) *Conveyer {
-
 	return &Conveyer{
-
 		mutex: sync.Mutex{},
 
 		channels: make(map[string]chan string),
@@ -30,33 +28,25 @@ func New(size int) *Conveyer {
 
 		size: size,
 	}
-
 }
 
 func (c *Conveyer) RegisterDecorator(
-
 	decorator func(context.Context, chan string, chan string) error,
 
 	input string,
 
 	output string,
-
 ) error {
-
 	c.mutex.Lock()
 
 	if _, exists := c.channels[input]; !exists {
-
 		c.channels[input] = make(chan string, c.size)
-
 	}
 
 	inCh := c.channels[input]
 
 	if _, exists := c.channels[output]; !exists {
-
 		c.channels[output] = make(chan string, c.size)
-
 	}
 
 	outCh := c.channels[output]
@@ -64,27 +54,21 @@ func (c *Conveyer) RegisterDecorator(
 	c.mutex.Unlock()
 
 	task := func(ctx context.Context) error {
-
 		return decorator(ctx, inCh, outCh)
-
 	}
 
 	c.tasks = append(c.tasks, task)
 
 	return nil
-
 }
 
 func (c *Conveyer) RegisterMultiplexer(
-
 	multiplexer func(context.Context, []chan string, chan string) error,
 
 	inputs []string,
 
 	output string,
-
 ) error {
-
 	inChs := make([]chan string, 0, len(inputs))
 
 	c.mutex.Lock()
@@ -92,9 +76,7 @@ func (c *Conveyer) RegisterMultiplexer(
 	for _, name := range inputs {
 
 		if _, exists := c.channels[name]; !exists {
-
 			c.channels[name] = make(chan string, c.size)
-
 		}
 
 		inChs = append(inChs, c.channels[name])
@@ -102,9 +84,7 @@ func (c *Conveyer) RegisterMultiplexer(
 	}
 
 	if _, exists := c.channels[output]; !exists {
-
 		c.channels[output] = make(chan string, c.size)
-
 	}
 
 	outCh := c.channels[output]
@@ -112,33 +92,25 @@ func (c *Conveyer) RegisterMultiplexer(
 	c.mutex.Unlock()
 
 	task := func(ctx context.Context) error {
-
 		return multiplexer(ctx, inChs, outCh)
-
 	}
 
 	c.tasks = append(c.tasks, task)
 
 	return nil
-
 }
 
 func (c *Conveyer) RegisterSeparator(
-
 	separator func(context.Context, chan string, []chan string) error,
 
 	input string,
 
 	outputs []string,
-
 ) error {
-
 	c.mutex.Lock()
 
 	if _, exists := c.channels[input]; !exists {
-
 		c.channels[input] = make(chan string, c.size)
-
 	}
 
 	inCh := c.channels[input]
@@ -148,9 +120,7 @@ func (c *Conveyer) RegisterSeparator(
 	for _, name := range outputs {
 
 		if _, exists := c.channels[name]; !exists {
-
 			c.channels[name] = make(chan string, c.size)
-
 		}
 
 		outChs = append(outChs, c.channels[name])
@@ -160,19 +130,15 @@ func (c *Conveyer) RegisterSeparator(
 	c.mutex.Unlock()
 
 	task := func(ctx context.Context) error {
-
 		return separator(ctx, inCh, outChs)
-
 	}
 
 	c.tasks = append(c.tasks, task)
 
 	return nil
-
 }
 
 func (c *Conveyer) Run(ctx context.Context) error {
-
 	var waitGroup sync.WaitGroup
 
 	errChan := make(chan error, len(c.tasks))
@@ -186,17 +152,12 @@ func (c *Conveyer) Run(ctx context.Context) error {
 		waitGroup.Add(1)
 
 		go func(work func(context.Context) error) {
-
 			defer waitGroup.Done()
 
 			err := work(ctxCancel)
-
 			if err != nil {
-
 				errChan <- err
-
 			}
-
 		}(task)
 
 	}
@@ -204,11 +165,9 @@ func (c *Conveyer) Run(ctx context.Context) error {
 	done := make(chan struct{})
 
 	go func() {
-
 		waitGroup.Wait()
 
 		close(done)
-
 	}()
 
 	var err error
@@ -228,35 +187,25 @@ func (c *Conveyer) Run(ctx context.Context) error {
 	c.closeAllChannels()
 
 	return err
-
 }
 
 func (c *Conveyer) closeAllChannels() {
-
 	c.mutex.Lock()
 
 	defer c.mutex.Unlock()
 
 	for _, channel := range c.channels {
-
 		func() {
-
 			defer func() {
-
 				_ = recover()
-
 			}()
 
 			close(channel)
-
 		}()
-
 	}
-
 }
 
 func (c *Conveyer) Send(input string, data string) error {
-
 	c.mutex.Lock()
 
 	targetChan, exists := c.channels[input]
@@ -264,19 +213,15 @@ func (c *Conveyer) Send(input string, data string) error {
 	c.mutex.Unlock()
 
 	if !exists {
-
 		return ErrChanNotFound
-
 	}
 
 	targetChan <- data
 
 	return nil
-
 }
 
 func (c *Conveyer) Recv(output string) (string, error) {
-
 	c.mutex.Lock()
 
 	targetChan, exists := c.channels[output]
@@ -284,17 +229,13 @@ func (c *Conveyer) Recv(output string) (string, error) {
 	c.mutex.Unlock()
 
 	if !exists {
-
 		return "", ErrChanNotFound
-
 	}
 
 	val, isOpen := <-targetChan
 
 	if !isOpen {
-
 		return "undefined", nil
-
 	}
 
 	return val, nil
