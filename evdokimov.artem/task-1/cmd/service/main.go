542fdf74a919db17dad40786364e6220b920f046
LAffey26 <anercontae25@gmail.com>
2026-01-06 19:54:01 +0300

    [TASK-1] fix

--- evdokimov.artem/task-1/cmd/service/main.go
+++ evdokimov.artem/task-1/cmd/service/main.go
@@ -1,44 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var a, b int
-	var op string
+	var (
+		num1, num2 int
+		sign       string
+	)
 
-	if _, err := fmt.Scan(&a); err != nil {
+	_, err := fmt.Scan(&num1)
+	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
 
-	if _, err := fmt.Scan(&op); err != nil {
-		fmt.Println("Invalid operation")
-		return
-	}
-
-	if _, err := fmt.Scan(&b); err != nil {
+	_, err = fmt.Scan(&num2)
+	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
 
-	var result int
-	switch op {
-	case "+":
-		result = a + b
-	case "-":
-		result = a - b
-	case "*":
-		result = a * b
-	case "/":
-		if b == 0 {
-			fmt.Println("Division by zero")
-			return
-		}
-		result = a / b
-	default:
+	_, err = fmt.Scan(&sign)
+	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
 
-	fmt.Println(result)
+	switch sign {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(num1 / num2)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
