c0b6ccae609ef4a45f60377532d7873ea732a166
Aleksandr Ryzhov <lasimerunes@mail.ru>
2026-01-08 02:08:21 +0300

    [TASK-2-2] First real commit

--- aleksandr.ryzhov/task-2-2/cmd/service/main.go
+++ aleksandr.ryzhov/task-2-2/cmd/service/main.go
@@ -10,7 +10,15 @@ type intHeap []int
 func (h *intHeap) Len() int           { return len(*h) }
 func (h *intHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
 func (h *intHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-func (h *intHeap) Push(x any)         { *h = append(*h, x.(int)) }
+func (h *intHeap) Push(x any) {
+	num, err := x.(int)
+	if !err {
+		fmt.Println("Incorrect number of departments", err)
+
+		return
+	}
+	*h = append(*h, num)
+}
 func (h *intHeap) Pop() any {
 	old := *h
 	n := len(old)
@@ -21,11 +29,44 @@ func (h *intHeap) Pop() any {
 }
 
 func main() {
-	meals := &intHeap{2, 1, 3, 4, 5}
-	heap.Init(meals)
-	heap.Push(meals, 10)
-	fmt.Printf("minimum: %d\n", (*meals)[0])
-	for meals.Len() > 0 {
-		fmt.Printf("%d ", heap.Pop(meals))
+	var dishesCount, prefDishNum, tmp int
+
+	// Получаем количество блюд
+	_, err := fmt.Scan(&dishesCount)
+	if err != nil || dishesCount < 1 {
+		fmt.Println("Incorrect number of meals ", err)
+
+		return
 	}
+
+	dishes := intHeap{}
+	heap.Init(&dishes)
+
+	// Получаем блюда по приоритетам
+	for range dishesCount {
+		_, err := fmt.Scan(&tmp)
+		if err != nil {
+			fmt.Println("Incorrect priority ", err)
+
+			return
+		}
+		heap.Push(&dishes, tmp)
+	}
+
+	// Получаем приоритет предпочитаемого блюда
+	_, err = fmt.Scan(&prefDishNum)
+	if err != nil {
+		fmt.Println("Incorrect priority ", err)
+
+		return
+	}
+
+	// Выбираем блюдо с нужным приоритетом
+	for range dishes {
+		if dishes.Len() == prefDishNum {
+			break
+		}
+		heap.Pop(&dishes)
+	}
+	fmt.Println(heap.Pop(&dishes))
 }
