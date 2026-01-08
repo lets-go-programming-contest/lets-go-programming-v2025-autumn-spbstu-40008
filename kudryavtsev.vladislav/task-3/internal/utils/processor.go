ec8f14bde8124a824e28a78363c3bd8b868edbf5
Vladislav <vldkudryavtsev@gmail.com>
2026-01-08 21:16:47 +0300

    [TASK-3] Automatic only

--- kudryavtsev.vladislav/task-3/internal/utils/processor.go
+++ kudryavtsev.vladislav/task-3/internal/utils/processor.go
@@ -7,13 +7,17 @@ import (
 )
 
 func SortCurrencyData(source *models.ExchangeData) []models.CurrencyItem {
+
 	output := make([]models.CurrencyItem, len(source.Items))
-	
+
 	copy(output, source.Items)
 
 	sort.Slice(output, func(i, j int) bool {
+
 		return output[i].Rate > output[j].Rate
+
 	})
 
 	return output
+
 }
