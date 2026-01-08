82ecf381df0314ea594a927880329d16903d8cc9
Vladislav <vldkudryavtsev@gmail.com>
2026-01-08 21:25:33 +0300

    [TASK-3] Automatic only

--- kudryavtsev.vladislav/task-3/internal/models/xml_types.go
+++ kudryavtsev.vladislav/task-3/internal/models/xml_types.go
@@ -10,29 +10,22 @@ import (
 type CustomFloat float64
 
 func (cf *CustomFloat) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-
 	var rawString string
 
 	if err := d.DecodeElement(&rawString, &start); err != nil {
-
 		return fmt.Errorf("element decode error: %w", err)
-
 	}
 
 	normalized := strings.ReplaceAll(rawString, ",", ".")
 
 	parsedVal, err := strconv.ParseFloat(normalized, 64)
-
 	if err != nil {
-
 		return fmt.Errorf("float conversion error: %w", err)
-
 	}
 
 	*cf = CustomFloat(parsedVal)
 
 	return nil
-
 }
 
 type ExchangeData struct {
