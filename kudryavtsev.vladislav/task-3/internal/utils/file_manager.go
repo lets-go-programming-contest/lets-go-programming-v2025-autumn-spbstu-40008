b562c4deb9a5f453fce9d86b8dde2e8c0246fb67
Vladislav <vldkudryavtsev@gmail.com>
2026-01-08 21:27:41 +0300

    [TASK-3] Automatic only

--- kudryavtsev.vladislav/task-3/internal/utils/file_manager.go
+++ kudryavtsev.vladislav/task-3/internal/utils/file_manager.go
@@ -13,13 +13,9 @@ import (
 )
 
 func LoadXML(filePath string) (*models.ExchangeData, error) {
-
 	fileContent, err := os.ReadFile(filePath)
-
 	if err != nil {
-
 		return nil, fmt.Errorf("cannot read source file %s: %w", filePath, err)
-
 	}
 
 	var result models.ExchangeData
@@ -31,41 +27,28 @@ func LoadXML(filePath string) (*models.ExchangeData, error) {
 	decoder.CharsetReader = charset.NewReaderLabel
 
 	err = decoder.Decode(&result)
-
 	if err != nil {
-
 		return nil, fmt.Errorf("xml parsing error: %w", err)
-
 	}
 
 	return &result, nil
-
 }
 
 func ExportToJSON(data []models.CurrencyItem, path string) error {
-
 	encodedJSON, err := json.MarshalIndent(data, "", " ")
-
 	if err != nil {
-
 		return fmt.Errorf("serialization error: %w", err)
-
 	}
 
 	directory := filepath.Dir(path)
 
 	if err := os.MkdirAll(directory, os.ModePerm); err != nil {
-
 		return fmt.Errorf("filesystem error (mkdir): %w", err)
-
 	}
 
 	if err := os.WriteFile(path, encodedJSON, 0o644); err != nil {
-
 		return fmt.Errorf("write error: %w", err)
-
 	}
 
 	return nil
-
 }
