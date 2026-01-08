c5f6baea9a92e6bbf0636c129017a5b0a74eabf6
prokopenko.nikita <nikita.prokopenko.kz@gmail.com>
2026-01-08 14:07:45 +0300

    [TASK-8] Automatic only

---
+++ nikita.prokopenko/task-8/cmd/service/go.mod
@@ -0,0 +1,5 @@
+module github.com/Czeeen/lets-go-programming-v2025-autumn-spbstu-40008/prokopenko.nikita/task-8
+
+go 1.22.7
+
+require gopkg.in/yaml.v3 v3.0.1
---
+++ nikita.prokopenko/task-8/internal/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+)
+
+type Settings struct {
+	AppStatus   string `yaml:"app_status"`
+	ReportLevel string `yaml:"report_level"`
+}
+
+func Load() (*Settings, error) {
+	var s Settings
+	if err := yaml.Unmarshal(yamlData, &s); err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+	return &s, nil
+}
