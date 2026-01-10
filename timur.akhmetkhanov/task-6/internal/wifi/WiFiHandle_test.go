670e6fa90be54be560d23210269f7d07f061af00
Timur Akhmetkhanov <t.akhmetkhanov@okko.tv>
2026-01-11 02:02:16 +0300

    [TASK-6] lint fix

--- timur.akhmetkhanov/task-6/internal/wifi/WiFiHandle_test.go
+++ timur.akhmetkhanov/task-6/internal/wifi/WiFiHandle_test.go
@@ -1,6 +1,8 @@
 package wifi_test
 
 import (
+	"fmt"
+
 	"github.com/mdlayher/wifi"
 	"github.com/stretchr/testify/mock"
 )
@@ -13,17 +15,20 @@ func (_m *MockWiFiHandle) Interfaces() ([]*wifi.Interface, error) {
 	ret := _m.Called()
 
 	var r0 []*wifi.Interface
-	if rf, ok := ret.Get(0).(func() []*wifi.Interface); ok {
-		r0 = rf()
+	if val, ok := ret.Get(0).([]*wifi.Interface); ok {
+		r0 = val
 	} else if ret.Get(0) != nil {
-		r0 = ret.Get(0).([]*wifi.Interface)
+		panic(fmt.Sprintf("unexpected type for return arg 0: %T", ret.Get(0)))
 	}
 
 	var r1 error
 	if rf, ok := ret.Get(1).(func() error); ok {
 		r1 = rf()
 	} else {
-		r1 = ret.Error(1)
+		err := ret.Error(1)
+		if err != nil {
+			r1 = fmt.Errorf("mock error: %w", err)
+		}
 	}
 
 	return r0, r1
