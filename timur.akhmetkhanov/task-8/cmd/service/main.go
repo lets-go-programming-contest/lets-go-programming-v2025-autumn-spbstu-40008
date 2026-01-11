6f12114a033703690432619bb86c28f22af15553
Timur Akhmetkhanov <t.akhmetkhanov@okko.tv>
2026-01-11 03:19:34 +0300

    [TASK-8] lint fix

---
+++ timur.akhmetkhanov/task-8/cmd/service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"task-8/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки конфига: %v", err)
+	}
+
+	fmt.Printf("%s %s\n", cfg.Environment, cfg.LogLevel)
+}
